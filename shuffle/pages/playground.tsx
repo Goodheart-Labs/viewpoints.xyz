50ee215f7109311f4d87456cbdc665f1ea7cb4f3
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-04-26 14:42:29 +0100

    shuffle: title and core question

--- shuffle/pages/playground.tsx
+++ shuffle/pages/playground.tsx
@@ -57,10 +57,20 @@ const fetchComments = async ({
 // };
 
 const Playground = () => (
-  <main
-    className={`relative flex flex-col justify-center items-center w-full h-screen gradient`}
-  >
-    <Cards />
+  <main className="flex flex-col items-center w-full h-screen gradient">
+    <div className="flex flex-col my-40 text-center max-w-[800px]">
+      <h1 className="mb-4 text-4xl font-bold text-black">Poly discussion</h1>
+      <h2 className="text-xl text-gray-800">
+        The poly discourse happens a lot, but I don&apos;t think we get concrete
+        enough. Here are some statements to vote on to try and see where we
+        agree and disagree.
+      </h2>
+    </div>
+    <div className="mt-20">
+      <div className="relative flex flex-col items-center justify-center w-full sm:min-w-[600px]">
+        <Cards />
+      </div>
+    </div>
     {/* <Fetch /> */}
   </main>
 );
