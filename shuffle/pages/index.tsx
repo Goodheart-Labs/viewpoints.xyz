b11d8cc8e1f875c7ad3e69f021bd6cd872722097
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-04-26 15:04:46 +0100

    shuffle: move into index

--- shuffle/pages/index.tsx
+++ shuffle/pages/index.tsx
@@ -1,8 +1,19 @@
+import Cards from "@/components/Cards";
+
 const Index = () => (
-  <main
-    className={`flex min-h-screen flex-col items-center justify-between p-24`}
-  >
-    <h1>Polislike</h1>
+  <main className="flex flex-col items-center w-full h-screen gradient">
+    <div className="flex flex-col my-40 text-center max-w-[800px]">
+      <h1 className="mb-4 text-4xl font-bold text-black">Poly discussion</h1>
+      <h2 className="text-xl text-gray-800">
+        The poly discourse happens a lot, but I don&apos;t think we get concrete
+        enough. Here are some statements to vote on to try and see where we
+        agree and disagree.
+      </h2>
+    </div>
+    <div className="mt-20">
+      <Cards />
+    </div>
+    {/* <Fetch /> */}
   </main>
 );
 
--- shuffle/pages/playground.tsx
+++ shuffle/pages/playground.tsx
@@ -1,4 +1,3 @@
-import Cards from "@/components/Cards";
 import apiConfig from "@/config/api";
 import axios from "axios";
 
@@ -58,17 +57,6 @@ const fetchComments = async ({
 
 const Playground = () => (
   <main className="flex flex-col items-center w-full h-screen gradient">
-    <div className="flex flex-col my-40 text-center max-w-[800px]">
-      <h1 className="mb-4 text-4xl font-bold text-black">Poly discussion</h1>
-      <h2 className="text-xl text-gray-800">
-        The poly discourse happens a lot, but I don&apos;t think we get concrete
-        enough. Here are some statements to vote on to try and see where we
-        agree and disagree.
-      </h2>
-    </div>
-    <div className="mt-20">
-      <Cards />
-    </div>
     {/* <Fetch /> */}
   </main>
 );
