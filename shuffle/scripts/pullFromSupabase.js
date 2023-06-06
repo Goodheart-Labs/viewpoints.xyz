fd1012933d393ff94a4375bb720907daafa615be
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-06-06 13:35:35 +0100

    shuffle: supabase script wip

--- shuffle/scripts/pullFromSupabase.js
+++ shuffle/scripts/pullFromSupabase.js
@@ -1,10 +1,13 @@
-require("dotenv").load();
-
 const { createClient } = require("@supabase/supabase-js");
 const { Pool } = require("pg");
-const supabase = createClient("your-supabase-url", "your-supabase-key");
 
-const connectionString = process.env.POSTGRES_PRISMA_URL;
+const supabase = createClient(
+  "https://yrehugaawjzlqnkoodru.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyZWh1Z2Fhd2p6bHFua29vZHJ1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MjY3MTg1NiwiZXhwIjoxOTk4MjQ3ODU2fQ.pHSKIWWzQeK8JkiBIgnq1J9huDbvq2Qq_CI5FUghznw"
+);
+
+const connectionString =
+  "postgres://viewpoints:viewpoints@localhost:5432/viewpoints_development";
 
 const pool = new Pool({
   connectionString,
@@ -50,62 +53,73 @@ async function fetchData() {
       }
     }
 
-    for (const poll of polls) {
-      await pool.query("INSERT INTO polls VALUES($1, $2, $3, $4, $5, $6, $7)", [
-        poll.id,
-        poll.polis_id,
-        poll.title,
-        poll.core_question,
-        poll.created_at,
-        poll.user_id,
-        poll.analytics_filters,
-      ]);
+    await pool.query("BEGIN");
 
-      for (const comment of poll.comments) {
-        await pool.query(
-          "INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+    try {
+      for (const poll of polls) {
+        const row = await pool.query(
+          "INSERT INTO polls (user_id,slug,title,core_question,created_at,analytics_filters) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id",
           [
-            comment.id,
-            comment.poll_id,
-            comment.user_id,
-            comment.edited_from_id,
-            comment.session_id,
-            comment.reporting_type,
-            comment.author_name,
-            comment.author_avatar_url,
-            comment.comment,
-            comment.created_at,
+            poll.user_id,
+            poll.polis_id,
+            poll.title,
+            poll.core_question,
+            poll.created_at,
+            poll.analytics_filters ?? "{}",
           ]
         );
 
-        for (const flaggedComment of comment.flagged_comments) {
-          await pool.query(
-            "INSERT INTO flagged_comments VALUES($1, $2, $3, $4, $5, $6)",
-            [
-              flaggedComment.id,
-              flaggedComment.comment_id,
-              flaggedComment.session_id,
-              flaggedComment.reason,
-              flaggedComment.created_at,
-              flaggedComment.user_id,
-            ]
-          );
-        }
+        const { id: pollId } = row;
+        console.log(row);
 
-        for (const response of comment.responses) {
-          await pool.query(
-            "INSERT INTO responses VALUES($1, $2, $3, $4, $5, $6)",
+        for (const comment of poll.comments) {
+          const { id: commentId } = await pool.query(
+            "INSERT INTO comments (poll_id,user_id,edited_from_id,session_id,reporting_type,author_name,author_avatar_url,comment,created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING id",
             [
-              response.id,
-              response.user_id,
-              response.comment_id,
-              response.session_id,
-              response.valence,
-              response.created_at,
+              pollId,
+              comment.user_id,
+              comment.edited_from_id,
+              comment.session_id,
+              comment.reporting_type,
+              comment.author_name,
+              comment.author_avatar_url,
+              comment.comment,
+              comment.created_at,
             ]
           );
+
+          for (const flaggedComment of comment.flagged_comments) {
+            await pool.query(
+              "INSERT INTO flagged_comments (comment_id,user_id,session_id,reason,created_at) VALUES ($1, $2, $3, $4, $5)",
+              [
+                commentId,
+                flaggedComment.user_id,
+                flaggedComment.session_id,
+                flaggedComment.reason,
+                flaggedComment.created_at,
+              ]
+            );
+          }
+
+          for (const response of comment.responses) {
+            await pool.query(
+              "INSERT INTO responses (user_id,comment_id,session_id,valence,created_at) VALUES ($1, $2, $3, $4, $5)",
+              [
+                response.user_id,
+                commentId,
+                response.session_id,
+                response.valence,
+                response.created_at,
+              ]
+            );
+          }
         }
       }
+    } catch (error) {
+      await pool.query("ROLLBACK");
+      throw error;
+    } finally {
+      await pool.query("COMMIT");
     }
 
     console.log("Data has been written to the local Postgres database.");
@@ -114,4 +128,9 @@ async function fetchData() {
   }
 }
 
-fetchData();
+fetchData()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
