20754607a908c26fa1e724e2a3b7d0d53868b72a
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-06-26 21:40:40 +0100

    shuffle: logrocket

--- shuffle/app/(frontend)/layout.tsx
+++ shuffle/app/(frontend)/layout.tsx
@@ -8,11 +8,20 @@ import "react-tooltip/dist/react-tooltip.css";
 import Contexts from "../components/Contexts";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
+import Script from "next/script";
+import Logrocket from "../components/Logrocket";
+
+// Metadata
+// -----------------------------------------------------------------------------
 
 export const metadata: Metadata = {
   title: "viewpoints.xyz",
   description: "what in the world are you thinking?",
 };
+
+// Default export
+// -----------------------------------------------------------------------------
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <ClerkProvider>
@@ -23,6 +32,7 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => (
           {children}
         </Contexts>
         <Tooltip id="tooltip" />
+        <Logrocket />
       </body>
     </html>
   </ClerkProvider>
