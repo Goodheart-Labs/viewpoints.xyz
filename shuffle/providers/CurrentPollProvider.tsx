0d5572c1cfa336bf7856d3503e1b62005bd144fc
turkish <petertorok@apexlab.io>
2023-08-10 11:06:45 +0200

    fix: stop requesting polls on new poll page

--- shuffle/providers/CurrentPollProvider.tsx
+++ shuffle/providers/CurrentPollProvider.tsx
@@ -48,6 +48,7 @@ const CurrentPollProvider = ({ children }: PropsWithChildren) => {
   // Fetch
 
   const currentPollQuery = useQuery(["polls", currentPollSlug], async () => {
+    if (pathname === "/polls/new") return null;
     if (!currentPollSlug) return undefined;
     const { data } = await axios.get(`/api/polls/${currentPollSlug}`);
     return data as Poll;
