ba38ac6395397f40d55ab94beabaf29aec784407
turkish <petertorok@apexlab.io>
2023-08-29 12:54:21 +0200

    fix: modify headers to make staging clerk work

--- shuffle/next.config.js
+++ shuffle/next.config.js
@@ -3,23 +3,25 @@ const nextConfig = {
   reactStrictMode: true,
   headers: async () => [
     {
-      source: "/iframe/:path*",
+      source: "/api/:path*",
       headers: [
         {
           key: "X-Frame-Options",
           value: "ALLOWALL",
         },
+        { key: "Access-Control-Allow-Credentials", value: "true" },
         {
           key: "Access-Control-Allow-Origin",
           value: "*",
         },
         {
           key: "Access-Control-Allow-Methods",
-          value: "GET",
+          value: "GET,DELETE,PATCH,POST,PUT",
         },
         {
           key: "Access-Control-Allow-Headers",
-          value: "*",
+          value:
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
         },
       ],
     },
