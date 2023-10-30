314afdf9c6800b4c63427c49c3403fd51afccaf9
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-10-30 16:57:39 +0000

    db: remove prisma from create statement api

--- app/api/responses/createResponse.tsx
+++ app/api/responses/createResponse.tsx
@@ -3,9 +3,9 @@
 import { auth } from "@clerk/nextjs";
 import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
-import prisma from "@/lib/prisma";
 import { SESSION_ID_COOKIE_NAME } from "@/middleware";
 import type { Response } from "@/db/schema";
+import { db } from "@/db/client";
 import { refreshPoll } from "../lib/refreshPoll";
 
 export const createResponse = async (
@@ -15,24 +15,25 @@ export const createResponse = async (
   const { userId } = auth();
   const sessionId = cookies().get(SESSION_ID_COOKIE_NAME)!.value;
 
-  const statement = await prisma.statement.findUnique({
-    where: {
-      id: statementId,
-    },
-  });
+  const statement = await db
+    .selectFrom("Statement")
+    .selectAll()
+    .where("id", "=", statementId)
+    .executeTakeFirst();
 
   if (!statement) {
     notFound();
   }
 
-  await prisma.responses.create({
-    data: {
+  await db
+    .insertInto("responses")
+    .values({
       statementId,
       session_id: sessionId,
       user_id: userId,
       choice,
-    },
-  });
+    })
+    .execute();
 
   await refreshPoll(statement.poll_id);
 };
