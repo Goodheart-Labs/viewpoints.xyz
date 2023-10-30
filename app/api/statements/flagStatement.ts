554f679054da9ebbf6ad2154ef33ff5c1ab50c32
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-10-30 16:19:48 +0000

    db: remove prisma from flag statement api

--- app/api/statements/flagStatement.ts
+++ app/api/statements/flagStatement.ts
@@ -1,13 +1,11 @@
 "use server";
 
 import { auth } from "@clerk/nextjs";
-import type { FlaggedStatement } from "@prisma/client";
+import type { FlaggedStatement } from "@/db/schema";
 import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
-
-import prisma from "@/lib/prisma";
+import { db } from "@/db/client";
 import { SESSION_ID_COOKIE_NAME } from "@/middleware";
-
 import { refreshPoll } from "../lib/refreshPoll";
 
 export const flagStatement = async (
@@ -17,24 +15,25 @@ export const flagStatement = async (
   const { userId } = auth();
   const sessionId = cookies().get(SESSION_ID_COOKIE_NAME)!.value;
 
-  const statement = await prisma.statement.findUnique({
-    where: {
-      id: statementId,
-    },
-  });
+  const statement = await db
+    .selectFrom("Statement")
+    .selectAll()
+    .where("id", "=", statementId)
+    .executeTakeFirst();
 
   if (!statement) {
     notFound();
   }
 
-  await prisma.flaggedStatement.create({
-    data: {
+  await db
+    .insertInto("FlaggedStatement")
+    .values({
       statementId: statement.id,
       user_id: userId,
       session_id: sessionId,
       ...data,
-    },
-  });
+    })
+    .execute();
 
   await refreshPoll(statement.poll_id);
 };
