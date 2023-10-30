b76569ea2670d9117f61220b3fff12531183efb7
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-10-30 19:07:37 +0000

    db: force sslmode=require

--- db/client.ts
+++ db/client.ts
@@ -2,6 +2,11 @@ import { Pool } from "pg";
 import { Kysely, PostgresDialect } from "kysely";
 import type { Database } from "./schema";
 
+let connectionString = process.env.DATABASE_URL;
+if (!connectionString?.endsWith("sslmode=require")) {
+  connectionString += "?sslmode=require";
+}
+
 const dialect = new PostgresDialect({
   pool: new Pool({
     connectionString: process.env.DATABASE_URL,
