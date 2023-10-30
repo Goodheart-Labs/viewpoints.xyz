5040fb778a1eb18fe1150224a3b78bfb105f77a7
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-10-30 16:22:32 +0000

    db: remove prisma from delete statement api; get rid of comment api

--- app/api/statements/deleteStatement.ts
+++ app/api/statements/deleteStatement.ts
@@ -1,18 +1,23 @@
 "use server";
 
 import { auth } from "@clerk/nextjs";
-
 import prisma from "@/lib/prisma";
 import { requirePollAdmin } from "@/utils/authutils";
-
+import { db } from "@/db/client";
+import { notFound } from "next/navigation";
 import { refreshPoll } from "../lib/refreshPoll";
 
 export const deleteStatement = async (pollId: number, statementId: number) => {
   const { userId } = auth();
 
-  const poll = await prisma.polls.findUnique({
-    where: { id: pollId },
-  });
+  const poll = await db
+    .selectFrom("polls")
+    .selectAll()
+    .where("id", "=", pollId)
+    .executeTakeFirst();
+  if (!poll) {
+    notFound();
+  }
 
   requirePollAdmin(poll, userId);
 
