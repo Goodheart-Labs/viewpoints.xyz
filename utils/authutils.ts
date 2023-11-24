05d2b3b2231a46cd8ef885efe9acfb67fb39d440
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-11-24 12:31:06 +0000

    utils: add debugging statements

--- utils/authutils.ts
+++ utils/authutils.ts
@@ -8,14 +8,30 @@ export const isPollAdmin = (
 ): poll is Poll => Boolean(userId && poll && poll.user_id === userId);
 
 export const requirePollAdmin = async (poll: Poll | null): Promise<boolean> => {
+  console.log("requirePollAdmin");
+
   const user = await currentUser();
+  console.log("requirePollAdmin:currentUser", { user });
+
   if (!user) {
+    console.log("requirePollAdmin - no user, notFound");
     return notFound();
   }
 
+  console.log("requirePollAdmin - is user super admin or poll admin?", {
+    isSuperAdmin: user.publicMetadata.isSuperAdmin,
+    isPollAdmin: isPollAdmin(poll, user.id),
+  });
+
   if (user.publicMetadata.isSuperAdmin || isPollAdmin(poll, user.id)) {
+    console.log("requirePollAdmin - user is super admin or poll admin, true");
+
     return true;
   }
+
+  console.log(
+    "requirePollAdmin - user is not super admin or poll admin, notFound",
+  );
 
   return notFound();
 };
