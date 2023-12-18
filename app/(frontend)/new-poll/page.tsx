6332fd11b402bfdd229f7dc784e6967b0bc17b38
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-12-18 20:54:28 +0000

    app: show me the headers

--- app/(frontend)/new-poll/page.tsx
+++ app/(frontend)/new-poll/page.tsx
@@ -1,24 +1,30 @@
 // import { auth } from "@clerk/nextjs";
 // import { notFound } from "next/navigation";
+import { headers } from "next/headers";
 import NewPollPageClient from "./client";
 
 // View
 // -----------------------------------------------------------------------------
 
-const NewPollPageView = () => (
+const NewPollPageView = () => {
   // const { userId } = auth();
   // if (!userId) notFound();
 
-  <main className="flex flex-col items-center w-full max-w-5xl min-h-screen px-4 mx-auto bg-black sm:px-0">
-    <div className="flex flex-col mt-10 sm:mt-40 mb-10 text-center max-w-[800px]">
-      <h1 className="mb-4 text-4xl font-bold dark:text-gray-200">
-        Create New Poll
-      </h1>
-    </div>
+  console.log("HEADERS", headers());
 
-    <NewPollPageClient />
-  </main>
-);
+  return (
+    <main className="flex flex-col items-center w-full max-w-5xl min-h-screen px-4 mx-auto bg-black sm:px-0">
+      <div className="flex flex-col mt-10 sm:mt-40 mb-10 text-center max-w-[800px]">
+        <h1 className="mb-4 text-4xl font-bold dark:text-gray-200">
+          Create New Poll
+        </h1>
+      </div>
+
+      <NewPollPageClient />
+    </main>
+  );
+};
+
 // Default export
 // -----------------------------------------------------------------------------
 
