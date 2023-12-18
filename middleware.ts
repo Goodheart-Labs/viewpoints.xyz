d92605961be5427aa6bc5cf6a301fa77aa0db964
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-12-18 23:35:24 +0000

    middleware: flush out cookies once again

--- middleware.ts
+++ middleware.ts
@@ -16,7 +16,7 @@ const publicRoutes = [
 
 export const SESSION_ID_COOKIE_NAME = "sessionId";
 export const CLEAR_LOCALSTORAGE_HEADER_NAME = "X-Clear-LocalStorage";
-export const AFTER_DEPLOY_COOKIE_NAME = "afterDeployWipe20231126";
+export const AFTER_DEPLOY_COOKIE_NAME = "afterDeployWipe20231218";
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
