ea57fe5399742277ca4e852f01a8d7defca0e24e
Jamie Rumbelow <jamie@jamierumbelow.net>
2023-12-18 20:28:01 +0000

    utils: frontend safe

--- utils/frontendsessionutils.ts
+++ utils/frontendsessionutils.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { SESSION_ID_COOKIE_NAME } from "@/middleware";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
@@ -5,10 +7,14 @@ import { getCookie, setCookie } from "typescript-cookie";
 
 export const useSessionId = () => {
   const [sessionId, setSessionId] = useState<string>(
-    getCookie(SESSION_ID_COOKIE_NAME) ?? "",
+    typeof document !== "undefined"
+      ? getCookie(SESSION_ID_COOKIE_NAME) ?? ""
+      : "",
   );
 
   useEffect(() => {
+    if (!document) return;
+
     const sessionCookie = getCookie(SESSION_ID_COOKIE_NAME);
     if (!sessionCookie) {
       const newSessionId = uuidv4();
