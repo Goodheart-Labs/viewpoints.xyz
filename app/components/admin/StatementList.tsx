fbd00a517b213c3dae064873ce9f332a32df31f1
Rob Gordon <rob.gordon@tone-row.com>
2024-03-26 06:51:40 -0400

    Add tooltip

--- app/components/admin/StatementList.tsx
+++ app/components/admin/StatementList.tsx
@@ -14,6 +14,7 @@ import type { FlaggedStatement, Poll, Statement } from "@/db/schema";
 import DeleteFlaggedStatementDialog from "./DeleteFlaggedStatementDialog";
 import DeleteStatementDialog from "./DeleteStatementDialog";
 import { StatementListButton } from "./StatementListButton";
+import { TooltipProvider } from "../shadcn/ui/tooltip";
 
 type Props = {
   poll: Poll;
@@ -43,71 +44,76 @@ const StatementsList: FC<Props> = ({ poll, statements, flaggedStatements }) => {
   };
 
   return (
-    <ScrollArea className="flex flex-col w-full px-6">
-      {statements.map((statement, index) => (
-        <div key={statement.id} className="mt-1 mb-2 ">
-          <div className="mb-2">
-            {(flaggedStatements[statement.id] ?? []).length > 1 && (
-              <span className="flex items-center text-xs bg-accent p-1.5 rounded-sm dark:text-foreground">
-                <FlagIcon width={10} height={10} className="mr-2" />
-                This statement has been removed from poll because of 2 reports.
-              </span>
-            )}
-          </div>
-
-          <div className="flex items-center justify-between">
-            <p className="mr-2 text-black dark:text-gray-200">
-              {statement.text}
-            </p>
-
-            <div className="flex gap-2">
+    <TooltipProvider>
+      <ScrollArea className="flex flex-col w-full px-6">
+        {statements.map((statement, index) => (
+          <div key={statement.id} className="mt-1 mb-2 ">
+            <div className="mb-2">
               {(flaggedStatements[statement.id] ?? []).length > 1 && (
+                <span className="flex items-center text-xs bg-accent p-1.5 rounded-sm dark:text-zinc-400">
+                  <FlagIcon width={10} height={10} className="mr-2" />
+                  This statement has been removed from poll because of 2
+                  reports.
+                </span>
+              )}
+            </div>
+
+            <div className="flex items-center justify-between">
+              <p className="mr-2 text-black dark:text-gray-200">
+                {statement.text}
+              </p>
+
+              <div className="flex gap-2">
+                {(flaggedStatements[statement.id] ?? []).length > 1 && (
+                  <StatementListButton
+                    onClick={() =>
+                      openDeleteFlaggedStatementsDialog(statement.id)
+                    }
+                    tooltip="Remove reports and restore statement"
+                  >
+                    <CornerUpLeftIcon
+                      width={12}
+                      height={12}
+                      className="w-4 h-4 text-foreground"
+                    />
+                  </StatementListButton>
+                )}
+
                 <StatementListButton
-                  onClick={() =>
-                    openDeleteFlaggedStatementsDialog(statement.id)
-                  }
+                  onClick={() => openDeleteStatementDialog(statement.id)}
+                  tooltip="Delete statement"
                 >
-                  <CornerUpLeftIcon
+                  <TrashIcon
                     width={12}
                     height={12}
                     className="w-4 h-4 text-foreground"
                   />
                 </StatementListButton>
-              )}
-
-              <StatementListButton
-                onClick={() => openDeleteStatementDialog(statement.id)}
-              >
-                <TrashIcon
-                  width={12}
-                  height={12}
-                  className="w-4 h-4 text-foreground"
-                />
-              </StatementListButton>
+              </div>
             </div>
+            {index !== statements.length - 1 && (
+              <Separator className="my-2 bg-muted" />
+            )}
           </div>
-          {index !== statements.length - 1 && (
-            <Separator className="my-2 bg-muted" />
-          )}
-        </div>
-      ))}
-      {selectedStatementId && modalType === "statement" && (
-        <DeleteStatementDialog
-          pollId={poll.id}
-          statementId={selectedStatementId}
-          isOpen={isOpen}
-          closeModal={() => setIsOpen(false)}
-        />
-      )}
-      {selectedStatementId && modalType === "flaggedStatement" && (
-        <DeleteFlaggedStatementDialog
-          isOpen={isOpen}
-          closeModal={() => setIsOpen(false)}
-          pollId={poll.id}
-          statementId={selectedStatementId}
-        />
-      )}
-    </ScrollArea>
+        ))}
+        {selectedStatementId && modalType === "statement" && (
+          <DeleteStatementDialog
+            pollId={poll.id}
+            statementId={selectedStatementId}
+            isOpen={isOpen}
+            closeModal={() => setIsOpen(false)}
+          />
+        )}
+        {selectedStatementId && modalType === "flaggedStatement" && (
+          <DeleteFlaggedStatementDialog
+            isOpen={isOpen}
+            closeModal={() => setIsOpen(false)}
+            pollId={poll.id}
+            statementId={selectedStatementId}
+          />
+        )}
+      </ScrollArea>
+    </TooltipProvider>
   );
 };
 
--- app/components/admin/StatementListButton.tsx
+++ app/components/admin/StatementListButton.tsx
@@ -1,11 +1,24 @@
-type ButtonProps = React.ComponentProps<"button">;
+"use client";
 
-export const StatementListButton = ({ children, ...props }: ButtonProps) => (
-  <button
-    className="bg-accent p-2.5 rounded-full focus:outline-none focus-visible:bg-neutral-700"
-    type="button"
-    {...props}
-  >
-    {children}
-  </button>
+import { Tooltip, TooltipContent, TooltipTrigger } from "../shadcn/ui/tooltip";
+
+type ButtonProps = React.ComponentProps<"button"> & { tooltip: string };
+
+export const StatementListButton = ({
+  children,
+  tooltip,
+  ...props
+}: ButtonProps) => (
+  <Tooltip>
+    <TooltipTrigger asChild>
+      <button
+        className="bg-accent p-2.5 rounded-full focus:outline-none focus-visible:bg-neutral-700"
+        type="button"
+        {...props}
+      >
+        {children}
+      </button>
+    </TooltipTrigger>
+    <TooltipContent>{tooltip}</TooltipContent>
+  </Tooltip>
 );
