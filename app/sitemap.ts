5eff30cea56fd4981007310c5294019413119814
Ammiel Yawson <ammielgyanyawson@gmail.com>
2024-05-14 07:02:28 +0000

    chore: setup seo

---
+++ app/sitemap.ts
@@ -0,0 +1,16 @@
+import type { MetadataRoute } from "next";
+import { getBaseUrl } from "@/utils/constants";
+import { db } from "@/db/client";
+
+export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
+  const polls = await getPolls();
+  return polls.map((poll) => ({
+    url: `${getBaseUrl()}/poll/${poll.slug}`,
+    priority: 0.5,
+    lastModified: new Date(poll.created_at!),
+  }));
+}
+
+async function getPolls() {
+  return db.selectFrom("polls").selectAll().execute();
+}
