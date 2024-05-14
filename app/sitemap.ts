ee528697e0f15de9c3e09cbb10743e0e746667ef
Ammiel Yawson <ammielgyanyawson@gmail.com>
2024-05-14 07:11:41 +0000

    fix: generate sitemap for only public polls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,12 +5,16 @@ import { db } from "@/db/client";
 export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
   const polls = await getPolls();
   return polls.map((poll) => ({
-    url: `${getBaseUrl()}/poll/${poll.slug}`,
+    url: `${getBaseUrl()}/poll/${poll}`,
     priority: 0.5,
     lastModified: new Date(poll.created_at!),
   }));
 }
 
 async function getPolls() {
-  return db.selectFrom("polls").selectAll().execute();
+  return db
+    .selectFrom("polls")
+    .selectAll()
+    .where("visibility", "=", "public")
+    .execute();
 }
