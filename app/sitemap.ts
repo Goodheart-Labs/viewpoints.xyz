8bd718b47825122f6ac7e79433ea338ec1570e41
Ammiel Yawson <ammielgyanyawson@gmail.com>
2024-05-14 07:16:24 +0000

    fix: use poll slug in sitemap urls

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ import { db } from "@/db/client";
 export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
   const polls = await getPolls();
   return polls.map((poll) => ({
-    url: `${getBaseUrl()}/poll/${poll}`,
+    url: `${getBaseUrl()}/poll/${poll.slug}`,
     priority: 0.5,
     lastModified: new Date(poll.created_at!),
   }));
