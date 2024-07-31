8543417f2fb7ca09a0fb6c454bda80b116f1dbac
Ammiel Yawson <ammielgyanyawson@gmail.com>
2024-07-31 15:33:46 +0000

    feat: revalidate polls page on poll creation

--- app/(frontend)/new-poll/page.tsx
+++ app/(frontend)/new-poll/page.tsx
@@ -37,7 +37,38 @@ export default async function NewPoll() {
           Create New Poll
         </h1>
       </div>
-      <NewPollPageClient canCreatePoll={canCreatePoll} />
+      <NewPollPageClient
+        canCreatePoll={canCreatePoll}
+        revalidateUserPolls={revalidateUserPolls}
+      />
     </main>
   );
 }
+
+import { revalidatePath } from "next/cache";
+
+export async function revalidateUserPolls() {
+  "use server";
+  revalidatePath("/user/polls");
+}
+
+// Example of how to use this action:
+//
+// 1. In a client component:
+// import { revalidateUserPolls } from './path/to/this/file';
+//
+// // Inside a component or event handler:
+// const handleSomeAction = async () => {
+//   // Perform some action that changes user polls
+//   await revalidateUserPolls();
+// };
+//
+// 2. In another server component:
+// import { revalidateUserPolls } from './path/to/this/file';
+//
+// // Inside the server component:
+// export default async function SomeServerComponent() {
+//   // Some logic that changes user polls
+//   await revalidateUserPolls();
+//   // Rest of the component
+// }
